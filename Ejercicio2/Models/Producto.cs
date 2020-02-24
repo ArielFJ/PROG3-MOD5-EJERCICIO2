42ba0796aa00d103dc64439c07dcfd1a881e2fa5
Ariel <arielfj@magnanym.com>
2020-02-24 19:52:05 -0400

    Modelos PRODUCTO, PROVEEDOR creados

---
+++ Ejercicio2/Models/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Producto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(125)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaVencimiento { get; set; }
+    
+        [Required]
+        public int IdProveedor { get; set; }
+    }
+}
---
+++ Ejercicio2/Models/Proveedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Proveedor
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [DataType(DataType.PhoneNumber)]
+        public string Num_telef { get; set; }
+    }
+}
